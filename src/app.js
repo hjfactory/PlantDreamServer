aee3df08a2a7fb51d619f105d643fc54596eba30
Humphrey <humphrey@woowahan.com>
2023-01-05 21:35:46 +0900

    미들웨어, 라우터 적용

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
-var bodyParser = require('body-parser');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const routeV1 = require('./routes/routeV1');
 
 const app = express();
-app.use(bodyParser.json())
 app.set('port', process.env.PORT || 8080);
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    res.send('Hello world!!');
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/v1', routeV1);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not found');
 });
 
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    res.send({msg: "success"});
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
